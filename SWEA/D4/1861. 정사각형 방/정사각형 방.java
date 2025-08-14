fee305c9381072c6078825891e7cca1f67468bdc
Jeongho <hjh4212@naver.com>
2025-08-14 15:53:50 +0900

    [D4] Title: 정사각형 방, Time: 130 ms, Memory: 34,272 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 class Solution {
 	static int N;
 	static int[][] numbers;
+	static int[][] visited;
 	static int max;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
@@ -22,6 +23,7 @@ class Solution {
 		for(int tc=1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			numbers = new int[N][N];
+			visited = new int[N][N];
 			start = Integer.MAX_VALUE;
 			max = 0;
 			
@@ -32,39 +34,35 @@ class Solution {
 				}
 			}
 			
+			
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					bfs(i, j);
-				}
-			}
-			sb.append("#").append(tc).append(" ").append(start).append(" ").append(max).append("\n");
-		}
-		System.out.println(sb);
-	}
-	static void bfs(int i, int j) {
-		q.clear();
-		q.offer(new Point(i, j, 1));
-		
-		while(!q.isEmpty()) {
-			Point cur = q.poll();
-			for(int dir=0; dir<4; dir++) {
-				int nr = cur.r + dr[dir];
-				int nc = cur.c + dc[dir];
-				int ndist = cur.dist + 1;
-				if(0<=nr && nr<N && 0<=nc && nc<N) {
-					if(numbers[cur.r][cur.c] + 1 == numbers[nr][nc]) {
-						q.offer(new Point(nr, nc, ndist));
-						if(ndist>=max) {
-							if(ndist==max) start = Math.min(start, numbers[i][j]);
-							else{
-								max = ndist;
-								start = numbers[i][j];
-							}
+					if(dfs(i, j) >= max) {
+						if(visited[i][j] == max) start = Math.min(start, numbers[i][j]);
+						else {
+							max = visited[i][j];
+							start = numbers[i][j];
 						}
 					}
 				}
 			}
+			sb.append("#").append(tc).append(" ").append(start==Integer.MAX_VALUE ? 1:start).append(" ").append(max).append("\n");
 		}
+		System.out.println(sb);
+	}
+	static int dfs(int i, int j) {
+		if(visited[i][j]!=0) return visited[i][j];
+		visited[i][j] = 1;
+		for(int dir=0; dir<4; dir++) {
+			int nr = i + dr[dir];
+			int nc = j + dc[dir];
+			if(0<=nr && nr<N && 0<=nc && nc<N) {
+				if(numbers[i][j] + 1 == numbers[nr][nc]) {
+					visited[i][j] = dfs(nr, nc) + 1;
+				}
+			}
+		}
+		return visited[i][j];
 	}
 	
 	static class Point {
