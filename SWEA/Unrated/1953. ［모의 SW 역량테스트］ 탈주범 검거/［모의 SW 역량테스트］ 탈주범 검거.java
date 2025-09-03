7ff92d39a66d805e958e05f6392cdf2a8281bc7c
Jeongho <hjh4212@naver.com>
2025-09-03 12:42:13 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 110 ms, Memory: 31,616 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,15 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
+// 메모리: 25,600kb 실행시간: 125ms
+
 public class Solution {
-    static int N, M, R, C, L;
+    static int N, M, R, C, L, answer;
     static int[][] map;
-    static int[][] visited;
+    static boolean [][] visited;
     // 각 dir에 대해 갈 수 있는 방향 설정
-    static int[][] dr = {{}, {-1, 1, 0, 0}, {-1, 1}, {0, 0}, {-1, 0}, {1, 0}, {1, 0}, {-1, 0}};
-    static int[][] dc = {{}, {0, 0, -1, 1}, {0, 0}, {-1, 1}, {0, 1}, {0, 1}, {0, -1}, {0, -1}};
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[][] dirs = {{}, {0, 1, 2, 3}, {0, 1}, {2, 3}, {0, 3}, {1, 3}, {1, 2}, {0, 2}};
+    static ArrayDeque<int[]> q = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,55 +31,45 @@ public class Solution {
             L = Integer.parseInt(st.nextToken());
 
             map = new int[N][M];
-            visited = new int[N][M];
+            visited = new boolean[N][M];
             for(int i=0; i<N; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j=0; j<M; j++){
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            visited[R][C] = 1;
-            int answer = 0;
-            dfs(R, C, 1);
-            for(int i=0; i<N; i++) {
-            	for(int j=0; j<M; j++) {
-            		if(visited[i][j] != 0) answer++;
-            	}
-            }
+            answer = 0;
+            bfs(R, C);
             sb.append("#").append(tc).append(" ").append(answer).append("\n");
         }
         System.out.println(sb);
     }
-    static void dfs(int r, int c, int cnt){
-    	// L번 이동하면 1 반환
-        if(cnt==L)return;
-        // 터널 종류
-        int dir = map[r][c];
-        for(int i=0; i<(dir==1?4:2); i++){
-        	// 다음 r, c 값 설정
-            int nr = r+dr[dir][i];
-            int nc = c+dc[dir][i];
-            // 범위, 방문 체크
-            if(nr<0 || N<=nr || nc<0 || M<=nc) continue;
-            if(visited[nr][nc]!=0 &&visited[nr][nc]<=cnt+1) continue;
-            // 다음 좌표에서 현재 좌표로 돌아올 수 있는지 체크
-        	if(!check(r, c, nr, nc)) continue;
-            visited[nr][nc] = cnt+1;
-            // 재귀로 n값 증가
-            dfs(nr, nc, cnt+1);
+    static void bfs(int r, int c){
+        visited[r][c] = true;
+        q.offer(new int[] {r, c, 1});
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cr = cur[0];
+            int cc = cur[1];
+            int cd = cur[2];
+            answer++;
+            if(cd==L) continue;
+            int[] dir = dirs[map[cr][cc]];
+            for(int i: dir){
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+                if(nr<0 || N<=nr || nc<0 || M<=nc) continue;
+                if(visited[nr][nc]) continue;
+                if(!check(i, nr, nc)) continue;
+                visited[nr][nc] = true;
+                q.offer(new int[] {nr, nc, cd+1});
+            }
         }
     }
-    static boolean check(int r, int c, int nr, int nc){
-    	// 다음 좌표의 터널 종류
-        int dir = map[nr][nc];
-        // 터널이 없으면 false 반환
-        if(dir==0) return false;
-        for(int i=0; i<(dir==1?4:2); i++){
-        	// 다음 좌표의 다음 좌표
-        	int nnr = nr + dr[dir][i];
-        	int nnc = nc + dc[dir][i];
-        	// 현재 좌표와 같다면 true 반환
-        	if(nnr==r && nnc==c) return true;
+    static boolean check(int d, int nr, int nc){
+        int[] dir = dirs[map[nr][nc]];
+        for(int nd: dir){
+            if(d+nd==1 || d+nd==5) return true;
         }
         return false;
     }
