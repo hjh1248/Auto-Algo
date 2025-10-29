369faf202c5f43cc5c7c1e81e8c00f0eb60d5583
Jeongho <hjh4212@naver.com>
2025-10-29 16:40:29 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 175 ms, Memory: 34,880 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,59 +1,64 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Solution {
-    public static void main(String[] args) throws IOException{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Solution {
+    static int N;
+    static int[][] map, dists;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
         int T = Integer.parseInt(br.readLine());
         for(int tc=1; tc<=T; tc++){
-            int N = Integer.parseInt(br.readLine());
-            int[][] map = new int[N][N];
-            int[][] dist = new int[N][N];
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            dists = new int[N][N];
+
+            for(int i=0; i<N; i++){
+                String line = br.readLine();
+                for(int j=0; j<N; j++){
+                    map[i][j] = line.charAt(j)-'0';
+                }                
+            }
             
             for(int i=0; i<N; i++){
-                char[] chars = br.readLine().toCharArray();
-                for(int j=0; j<N; j++){
-                    map[i][j] = chars[j] - '0';
-                    dist[i][j] = Integer.MAX_VALUE;
-                }
+                Arrays.fill(dists[i], Integer.MAX_VALUE);
             }
-            dist[0][0] = 0;
-            pq.offer(new Node(0, 0, 0));
-            while(!pq.isEmpty()){
-                Node cur = pq.poll();
-                if(cur.d > dist[cur.r][cur.c]) continue;
-                for(int i=0; i<4; i++){
-                    int nr = cur.r + dr[i];
-                    int nc = cur.c + dc[i];
-                    if(nr<0 || N<=nr || nc<0 || N<=nc) continue;
-                    int nd = cur.d + map[nr][nc];
-                    if(dist[nr][nc] <= nd) continue;
-                    dist[nr][nc] = nd;
-                    pq.offer(new Node(nr, nc, nd));
-                }
-            }
-            int answer = dist[N-1][N-1];
-            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+            
+            dijkstra(0, 0);
+            sb.append("#").append(tc).append(" ").append(dists[N-1][N-1]).append("\n");
         }
         System.out.println(sb);
     }
-    static class Node implements Comparable<Node>{
-        int r, c, d;
-        Node(int r, int c, int d){
-            this.r = r;
-            this.c = c;
-            this.d = d;
-        }
-        public int compareTo(Node o){
-            return this.d - o.d;
+    static void dijkstra(int r, int c){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        dists[r][c] = 0;
+        pq.offer(new int[] {r, c, 0});
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int cr = cur[0];
+            int cc = cur[1];
+            int dist = cur[2];
+
+            if(dists[cr][cc]<dist) continue;
+
+            for(int i=0; i<4; i++){
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+                if(nr<0 || nr>=N || nc<0 || nc>=N) continue;
+                int nDist = dist + map[nr][nc];
+                if(nDist >= dists[nr][nc]) continue;
+                dists[nr][nc] = nDist;
+                pq.offer(new int[] {nr, nc, nDist});
+            }
         }
     }
 }
