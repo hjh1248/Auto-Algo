eeaa1a3cc6a947fc19833c0d5115466648c52933
Jeongho <hjh4212@naver.com>
2025-11-05 11:05:52 +0900

    [Silver II] Title: 병사 배치하기, Time: 140 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -12,26 +12,18 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
-        int[][] dp = new int[N][2];
+        int[] dp = new int[N];
 
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=1; i<N; i++){
+        for(int i=0; i<N; i++){
             for(int j=0; j<i; j++){
                 if(arr[j] <= arr[i]) continue;
-                dp[i][0] = Math.max(dp[i][0], dp[j][0]+1);
+                dp[i] = Math.max(dp[i], dp[j]+1);
             }
-        }
-        for(int i=N-2; i>=0; i--){
-            for(int j=N-1; j>i; j--){
-                if(arr[j] >= arr[i]) continue;
-                dp[i][1] = Math.max(dp[i][1], dp[j][1]+1);
-            }
-        }
-        for(int i=0; i<N; i++){
-            answer = Math.max(answer, dp[i][0] + dp[i][1] +1);
+            answer = Math.max(answer, dp[i]+1);
         }
         System.out.println(N-answer);
     }
